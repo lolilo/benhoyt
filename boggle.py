78168bf000eb996befbe658396a295ff46061a73
Hackbright Student <info@hackbrightacademy.com>
2014-09-16 17:30:11 -0700

    understood

--- boggle.py
+++ boggle.py
@@ -73,16 +73,30 @@ def is_prefix(prefix):
         node = node[letter]
     return True
 
+# given a dictionary, prefixes, and a Boggle board, find all viable words in the board
+# (x, y)
+# (0, 0) is the left-top most corner
+# T N N H A
+# I G N E I
+# S G E I B
+# C A H N I
+# O N F O E
+
+# board is in the form 
+# {(1, 3): 'a', (3, 0): 'h', (2, 1): 'n', (0, 3): 'c', (4, 0): 'a', (1, 2): 'g', (3, 3): 'n', (4, 4): 'e', (0, 4): 'o', (4, 1): 'i', (1, 1): 'g', (3, 2): 'i', (0, 0): 't', (2, 2): 'e', (1, 4): 'n', (2, 3): 'h', (4, 2): 'b', (1, 0): 'n', (0, 1): 'i', (3, 1): 'e', (2, 4): 'f', (2, 0): 'n', (4, 3): 'i', (3, 4): 'o', (0, 2): 's'}
+
+
+# returns a set of found words
 def find_words(board, positions_used, prefix, pos):
-    current = prefix + board[pos]
-    if not is_prefix(current):
-        # No words with this as a prefix
-        return set()
+    prefix = prefix + board[pos] # update prefix
+    if not is_prefix(prefix):
+        # no words with this as a prefix
+        return set() # return an empty set
 
     found = set()
-    if current in DICTIONARY:
-        found.add(current)
-    positions_used.add(pos)
+    if prefix in DICTIONARY:
+        found.add(prefix)
+    positions_used.add(pos) # keep track of which coordinates we've visited
 
     for offset in OFFSETS:
         new_pos = (pos[0] + offset[0], pos[1] + offset[1])
@@ -91,20 +105,20 @@ def find_words(board, positions_used, prefix, pos):
         if not (0 <= new_pos[0] < WIDTH and 0 <= new_pos[1] < HEIGHT):
             continue
 
-        found.update(find_words(board, positions_used, current, new_pos))
+        found.update(find_words(board, positions_used, prefix, new_pos))
 
-    positions_used.remove(pos)
+    positions_used.remove(pos) # reset for use in other recursive calls
     return found
 
 def solve(board):
     words = set()
     for y in range(HEIGHT):
         for x in range(WIDTH):
-            found = find_words(board, set(), '', (x, y))
+            found = find_words(board, set(), '', (x, y)) # initialize empty set and prefix for first call
             words.update(found) # update set words with elements from set found
     return words
 
-def make_board(letters=None):
+def make_board(letters=None): 
     if letters is None:
         cubes = list(CUBES)
         random.shuffle(cubes)
@@ -146,13 +160,14 @@ Example: %prog  T N N H A  I G N E I  S G E I B  C A H N I  O N F O E"""
         board = make_board()
 
     print_board(board)
+    print board
     print '----'
     words = solve(board)
     total_score = 0
     for word in sorted(words):
         word_score = SCORES.get(len(word), SCORES[None])
         total_score += word_score
-        print word
+        # print word
 
     print '----'
     print 'Total score:', total_score, 'from', len(words), 'words'
